9d980356e7de00b0b4bd202538fc7c4fda8da3f2
skywalker <skywalker_x@163.com>
2017-10-10 17:47:22 +0800

    获取模板ID

---
+++ WxBookCode/WxApi/MsgEntity/TemplateID.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WxApi.ReceiveEntity;
+
+namespace WxApi.MsgEntity
+{
+    public class TemplateID :ErrorEntity
+    {
+        public string template_id { get; set; }
+    }
+}
--- WxBookCode/WxApi/MsgEntity/TemplateNotice.cs
+++ WxBookCode/WxApi/MsgEntity/TemplateNotice.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using WxApi.ReceiveEntity;
 
 namespace WxApi.MsgEntity
 {
@@ -18,6 +19,32 @@ namespace WxApi.MsgEntity
                     _industryList = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, int>>>(Code.IndustryCode);
                 return _industryList;
             }
+        }
+
+        /// <summary>
+        /// 设置公众号所属行业
+        /// </summary>
+        /// <param name="id1"></param>
+        /// <param name="id2"></param>
+        /// <param name="accessToken"></param>
+        /// <returns></returns>
+        public static ErrorEntity SetIndustry(string id1, string id2, string accessToken)
+        {
+            var url = string.Format("https://api.weixin.qq.com/cgi-bin/templates/api_get_industry?access_token={0}", accessToken);
+            var json = new { industry_id1 = id1, industry_id2 = id2 };
+            return Utils.PostResult<ErrorEntity>(json, url);
+        }
+        /// <summary>
+        /// 获取模板ID
+        /// </summary>
+        /// <param name="templateNo">模板编号</param>
+        /// <param name="accessToken">accessToken</param>
+        /// <returns></returns>
+        public static TemplateID GetTemplatedId(string templateNo, string accessToken)
+        {
+            var url = string.Format("https://api.weixin.qq.com/cgi-bin/template/api_add_template?access_token={0}", accessToken);
+            var json = new { template_id_short = templateNo };
+            return Utils.PostResult<TemplateID>(json, url);
         }
     }
 }
